0b8f67e4c55cf111070db0f7a49b1714fa0d0703
grigor peshin <linashikhova@mail.ru>
2025-09-29 09:19:11 +0500

    Разработанный класс Channel.Kazakov

---
+++ Classes/Channel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TV_Kazakov.Classes
+{
+    public class Channel
+    {
+        public string Name { get; set; }
+
+        public string Src { get; set; }
+    }
+}
